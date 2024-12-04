5fddfc2f7f2ff220e754d2ce510a51a58d906a00
b01tech <bruno01.tech@gmail.com>
2024-12-04 17:31:26 -0300

    feat: add base and user entities

---
+++ src/Cadastro.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Cadastro.Domain.Entities;
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; } = true;
+}
---
+++ src/Cadastro.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Cadastro.Domain.Entities;
+public class User : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string AreaCode { get; set; } = string.Empty;
+}
