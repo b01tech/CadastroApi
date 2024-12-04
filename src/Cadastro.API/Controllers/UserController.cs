e30f1904a43cf06f656eb252dd439307df7aff07
b01tech <bruno01.tech@gmail.com>
2024-12-04 16:54:02 -0300

    initial commit

---
+++ src/Cadastro.API/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cadastro.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
